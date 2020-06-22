d7fec69357dd524c331dfa68bc7d8dd1c48366f9
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-06-23 00:23:53 +0900

    add build

---
+++ build/precache-manifest.2872cfe93c175d03f16ae8e1f02208c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc57f5f93a91eee5a2a58fc6b4750dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d795fd1fd5a19349e0d7",
+    "url": "/static/js/2.67f1eef2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67f1eef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06beb6e7e3f05d1d6bfd",
+    "url": "/static/js/main.1e2e6fe4.chunk.js"
+  },
+  {
+    "revision": "94daa3620373d32b7bbe",
+    "url": "/static/js/runtime-main.debca5c0.js"
+  },
+  {
+    "revision": "129b12d2a88a0746b992808244e3345f",
+    "url": "/static/media/good.129b12d2.svg"
+  },
+  {
+    "revision": "b032e7e6b96dedda8bdcf34bcf6fab6a",
+    "url": "/static/media/heart.b032e7e6.svg"
+  },
+  {
+    "revision": "e23ec2c5d0170033e3120d877889e554",
+    "url": "/static/media/man1.e23ec2c5.jpg"
+  },
+  {
+    "revision": "b94c2a638e9306e2c3b32440912daedf",
+    "url": "/static/media/man2.b94c2a63.jpg"
+  }
+]);
